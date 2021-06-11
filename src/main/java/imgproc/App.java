c22c4ed77e328f0a6b52fe08d7c41a4f0d1341ba
Sgodd <40842209+Sgodd@users.noreply.github.com>
2021-06-11 17:30:43 +0100

    Removed unecessary lines

--- src/main/java/imgproc/App.java
+++ src/main/java/imgproc/App.java
@@ -9,6 +9,7 @@ import imgproc.nodes.dither.DitherNode;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import nodelibrary.editor.Editor;
+import nodelibrary.editor.view.NodeMenu;
 
 /**
  * Hello world!
@@ -24,7 +25,18 @@ public class App extends Application
     public void start(Stage stage) {
         Editor editor = new Editor(stage);
 
-        
+        NodeMenu input = editor.menu.createSubMenu("Input");
+        input.addMenuItem("Image", ImageNode.class);
+
+        NodeMenu output = editor.menu.createSubMenu("Output");
+        output.addMenuItem("Viewer", ImageViewerNode.class);
+
+        NodeMenu filters = editor.menu.createSubMenu("Filters");
+        filters.addMenuItem("Sobel Filter", SobelFilterNode.class);
+
+        editor.menu.addMenu(input);
+        editor.menu.addMenu(output);
+        editor.menu.addMenu(filters);
 
         // editor.addMenuItem("Image", ImageNode.class);
         // editor.addMenuItem("Image Viewer", ImageViewerNode.class);
--- src/main/java/nodelibrary/editor/view/EditorCanvas.java
+++ src/main/java/nodelibrary/editor/view/EditorCanvas.java
@@ -1,8 +1,6 @@
 package nodelibrary.editor.view;
 
 import javafx.scene.Group;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import nodelibrary.editor.node.Node;
 import nodelibrary.editor.node.components.sockets.SocketController;
@@ -12,56 +10,28 @@ public class EditorCanvas extends AnchorPane {
 
     public final EditorScene scene;
     private Group nodes;
-    // private ContextMenu menu = new ContextMenu();
-
-    private double mx, my = 0;
 
     public EditorCanvas() {
         scene = new EditorScene();
         nodes = new Group();
-        // menu = new NodeMenu(this);
 
-        setTopAnchor(scene, 0.0);
-        setBottomAnchor(scene, 0.0);
-        setLeftAnchor(scene, 0.0);
-        setRightAnchor(scene, 0.0);
+        AnchorPane.setTopAnchor(scene, 0.0);
+        AnchorPane.setBottomAnchor(scene, 0.0);
+        AnchorPane.setLeftAnchor(scene, 0.0);
+        AnchorPane.setRightAnchor(scene, 0.0);
 
         getChildren().addAll(scene, SocketController.MAIN, SocketController.MAIN.guideLine(), nodes);
 
-        scene.setOnContextMenuRequested(e -> {
-
-            // menu.setPos(e.getSceneX(), e.getSceneY());
-            // menu.show(scene, e.getScreenX(), e.getScreenY());
-            // mx = e.getSceneX();
-            // my = e.getSceneY();
-            // menu.show(scene, e.getScreenX(), e.getScreenY());
-
-            // e.consume();
-        });
-
         addEventHandler(NodeEvent.DELETED, e -> {
-
             nodes.getChildren().remove(e.node);
 
             e.consume();
         });
+
+        scene.setOnMouseDragged(e -> {
+            
+        });
     }
-
-    // public <T extends Node> void addMenuItem(String itemLabel, Class<T> type) {
-    // MenuItem mItem = new MenuItem(itemLabel);
-
-    // mItem.setOnAction(e -> {
-    // try {
-    // T node = type.getDeclaredConstructor(double.class,
-    // double.class).newInstance(mx, my);
-    // addNode(node);
-    // } catch (Exception err) {
-    // err.printStackTrace();
-    // }
-    // });
-
-    // menu.getItems().add(mItem);
-    // }
 
     public void addNode(Node node) {
         nodes.getChildren().add(node);
